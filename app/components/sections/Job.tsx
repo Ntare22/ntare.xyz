2ae71a98a08a037c1fb0ef0472f25353201bddf8
Ntare22 <jim.ntare@gmail.com>
2024-12-14 20:35:20 +0300

    deploy

--- app/components/sections/Job.tsx
+++ app/components/sections/Job.tsx
@@ -17,7 +17,7 @@ export default function Job({
     <div className={`${manrope.className} py-2`}>
       <div className="flex flex-col py-2 lg:flex-row lg:justify-between">
         <h1 className={`${manrope.className} font-thin`}>{role}</h1>
-        <p className={`${manrope.className} text-[#C7C7C7]`}>{`${moment(start_date).format("MMMM, YYYY")} - ${!end_date ? 'Present' : moment(start_date).format("MMMM, YYYY")}`}</p>
+        <p className={`${manrope.className} text-[#C7C7C7]`}>{`${moment(start_date).format("MMMM, YYYY")} - ${!end_date ? 'Present' : moment(end_date).format("MMMM, YYYY")}`}</p>
       </div>
       <p className={`${manrope.className} py-3 text-[#D3E97A]`}>{company}</p>
       <p className="font-thin text-[#C7C7C7]">{description}</p>

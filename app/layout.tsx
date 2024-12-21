2b52b326c5e7f7dd9bd06916b17639e7276e343a
Ntare22 <jim.ntare@gmail.com>
2024-12-21 10:02:13 +0300

    deploy analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 import Navbar from "./components/sections/Navbar";
 import ApolloProviderWrapper from "./lib/apolloClient";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
         <ApolloProviderWrapper>
           <Navbar />
           {children}
+          <Analytics />
         </ApolloProviderWrapper>
       </body>
     </html>
